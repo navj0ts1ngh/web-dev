9fb4d65d7c0c4900b1e6dae2113803a33c13ebcf
navj0ts1ngh <ai.navjotsingh@gmail.com>
2026-02-14 03:22:23 +0530

    I have just mergerged changes from feature/a  branch

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ function add(a, b){
     return a + b
 }
 
+function sub(a,b){
+    if (!(a > b)) throw new Error ('Number a must be greater than b')
+    
+    return a-b
+}
+
 add(2, 5)
 add(10, 23)
 add(20,30)
+
+sub(10,3)
+
