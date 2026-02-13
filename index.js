9b22d45d505a6421d4ac20d33cde460097b73743
navj0ts1ngh <ai.navjotsingh@gmail.com>
2026-02-14 03:44:26 +0530

    Feat/multiply (#1)
    * Add multiply function to index.js
    
    * Add multiply function call to index.js
    
    * Add another multiply function call to index.js
    
    * Add another multiply function call to index.js
    
    * Add another multiply function call to index.js
    
    * Implement validation in multiply function to disallow negative numbers

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ function sub(a,b){
     return a-b
 }
 
+function multiply(a, b){
+    if(a < 0 || b < 0) throw Error('No negative numbers allowed') 
+    return a*b
+}
+
 add(2, 5)
 add(10, 23)
 add(20,30)
 
 sub(10,3)
+multiply(14,27)
+multiply(20,35)
+multiply(7,9)
+multiply(7,99)
+
 
